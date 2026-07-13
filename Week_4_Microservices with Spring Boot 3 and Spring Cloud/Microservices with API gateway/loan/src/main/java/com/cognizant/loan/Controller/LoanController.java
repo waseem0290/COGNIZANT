5e49d48b264bfa1fd286bd63ba193b09581afa56
waseem0290 <123884676+waseem0290@users.noreply.github.com>
2026-07-13 19:11:56 +0530

    Add files via upload

---
+++ Week_4_Microservices with Spring Boot 3 and Spring Cloud/Microservices with API gateway/loan/src/main/java/com/cognizant/loan/Controller/LoanController.java
@@ -0,0 +1,13 @@
+package com.cognizant.loan.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import com.cognizant.loan.model.Loan;
+@RestController
+public class LoanController {
+    @GetMapping("/loans/{number}")
+    public Loan getLoan(@PathVariable String number) {
+        return new Loan(number, "Car", 400000, 3258, 18);
+    }
+}
