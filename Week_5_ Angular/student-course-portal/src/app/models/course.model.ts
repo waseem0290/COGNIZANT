2ba98df48d8b7d146a9039409d0edec1df05a0f5
waseem0290 <123884676+waseem0290@users.noreply.github.com>
2026-07-24 19:17:42 +0530

    Delete Week_5_ Angular directory

--- Week_5_ Angular/student-course-portal/src/app/models/course.model.ts
+++
@@ -1,8 +0,0 @@
-export interface Course {
-  id: number;
-  name: string;
-  code: string;
-  credits: number;
-  gradeStatus:
-  'passed' | 'failed' | 'pending';
-}
--- Week_5_ Angular/student-course-portal/src/app/pipes/credit-label-pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CreditLabelPipe } from './credit-label-pipe';
-
-describe('CreditLabelPipe', () => {
-  it('create an instance', () => {
-    const pipe = new CreditLabelPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
